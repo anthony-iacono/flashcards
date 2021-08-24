d77c3632118e35bf62acb06bda514141efa9604f
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-08-24 12:09:27 -0700

    Add more assertions to evaluation and feedback

--- test/Turn-test.js
+++ test/Turn-test.js
@@ -5,32 +5,46 @@ const Turn = require('../src/Turn');
 const Card = require('../src/Card');
 
 describe('Turn', function () {
-  const card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
-  const turn = new Turn('object', card);
+  let card;
+  let turn;
+
+  beforeEach(function() {
+    card = new Card(1, 'What allows you to define a set of related information using key-value pairs?', ['object', 'array', 'function'], 'object');
+    turn = new Turn('object', card);
+  });
 
   it('should take users\'s guess and current card as arguments', function() {
     expect(turn.guess).to.equal('object');
     expect(turn.card).to.equal(card);
   });
 
-  it('should return guess', function() {
+  it('should be able to return user\'s guess', function() {
     const guess = turn.returnGuess();
+
     expect(guess).to.equal('object');
   });
 
-  it('should return card', function() {
+  it('should be able to return current card', function() {
     const returnedCard = turn.returnCard();
+
     expect(returnedCard.id).to.equal(card.id);
   });
 
-  it('should check guess against correct answer', function() {
-    const guessEvaluation = turn.evaluateGuess();
-    expect(guessEvaluation).to.equal(true);
-  });  
+  it('should be able to determine whether guess is correct or incorrect', function() {
+    const correctGuess = turn.evaluateGuess();
+    turn.guess = 'function';
+    const incorrectGuess = turn.evaluateGuess();
 
-  it('should give feedback', function() {
-    const feedback = turn.giveFeedback();
-    expect(feedback).to.equal('correct!');
+    expect(correctGuess).to.equal(true);
+    expect(incorrectGuess).to.equal(false);
   });
 
+  it('should give feedback', function() {
+    const positiveFeedback = turn.giveFeedback();
+    turn.guess = 'function';
+    const negativeFeedback = turn.giveFeedback();
+
+    expect(positiveFeedback).to.equal('correct!');
+    expect(negativeFeedback).to.equal('incorrect!');
+  });
 });
