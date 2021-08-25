f4ff96809e5c70ac4393df4937b5d538bf880aac
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-08-24 19:17:46 -0700

    Test for first card match

--- test/Deck-test.js
+++ test/Deck-test.js
@@ -8,7 +8,7 @@ describe('Deck', function() {
   const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
   const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
   const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-  const cards = [card1, card2, card3]
+  const cards = [card1, card2, card3];
   const deck = new Deck(cards);
 
   it('should take an array of cards as an argument', function() {
--- test/Round-test.js
+++ test/Round-test.js
@@ -4,3 +4,20 @@ const expect = chai.expect;
 const Card = require('../src/Card');
 const Deck = require('../src/Deck');
 const Turn = require('../src/Turn');
+
+describe('Round', function() {
+  const card1 = new Card(1, 'What is Robbie\'s favorite animal', ['sea otter', 'pug', 'capybara'], 'sea otter');
+  const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+  const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+  let deck;
+  let round;
+
+  beforeEach(function() {
+    deck = new Deck([card1, card2, card3]);
+    round = new Round(deck);
+  })
+
+  it('should hold the first card in the deck as the current card at the start of the round', function() {
+    expect(round.currentCard).to.equal.(card1);
+  });
+});
