895e20016d63e9093c4d283376dede3087910cdb
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-08-25 21:27:19 -0700

    Create Gamer start method

--- src/Game.js
+++ src/Game.js
@@ -1,14 +1,22 @@
 const data = require('./data');
-const prototypeQuestions = data.prototypeData;
+const questions = data.prototypeData;
 const util = require('./util');
+const Card = require('./Card');
+const Deck = require('./Deck');
+const Turn = require('./Turn');
+const Round = require('./Round');
 
 class Game {
   constructor() {
-    // this.currentRound = round;
+    this.currentRound;
   }
 
-  start(prototypeQuestions) {
-
+  start() {
+    const cards = questions.map((question) => new Card(question.id, question.question, question.answers, question.correctAnswer));
+    const deck = new Deck(cards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
   }
 
   printMessage(deck, round) {
--- test/Game-test.js
+++ test/Game-test.js
@@ -8,28 +8,45 @@ const Round = require('../src/Round');
 const Game = require('../src/Game');
 
 describe('Game', function() {
-  const questions = [{
-    "id": 1,
-    "question": "What is Robbie\'s favorite animal?",
-    "answers": ["sea otter", "pug", "capybara"],
-    "correctAnswer": "sea otter"
-  }, {
-    "id": 14,
-    "question": "What organ is Khalid missing?",
-    "answers": ["spleen", "appendix", "gallbladder"],
-    "correctAnswer": "gallbladder"
-  }, {
-    "id": 12,
-    "question": "What is Travis\'s middle name?",
-    "answers": ["Lex", "William", "Fitzgerald"],
-    "correctAnswer": "Fitzgerald"
-  }];
-  const card1 = new Card(1, 'What is Robbie\'s favorite animal?', ['sea otter', 'pug', 'capybara'], 'sea otter');
-  const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
-  const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
-  const game = new Game;
-  const deck = new Deck([card1, card2, card3]);
-  let round;
+  let game;
+
+  beforeEach(function() {
+    game = new Game;
+    game.start();
+  })
+
+  it('should store the current round', function() {
+    expect(game.currentRound).to.be.an.instanceof(Round);
+  })
+
+  // describe('start', function() {
+  //
+  //
+  // }
+
+
+  // const questions = [{
+  //   "id": 1,
+  //   "question": "What is Robbie\'s favorite animal?",
+  //   "answers": ["sea otter", "pug", "capybara"],
+  //   "correctAnswer": "sea otter"
+  // }, {
+  //   "id": 14,
+  //   "question": "What organ is Khalid missing?",
+  //   "answers": ["spleen", "appendix", "gallbladder"],
+  //   "correctAnswer": "gallbladder"
+  // }, {
+  //   "id": 12,
+  //   "question": "What is Travis\'s middle name?",
+  //   "answers": ["Lex", "William", "Fitzgerald"],
+  //   "correctAnswer": "Fitzgerald"
+  // }];
+  // const card1 = new Card(1, 'What is Robbie\'s favorite animal?', ['sea otter', 'pug', 'capybara'], 'sea otter');
+  // const card2 = new Card(14, 'What organ is Khalid missing?', ['spleen', 'appendix', 'gallbladder'], 'gallbladder');
+  // const card3 = new Card(12, 'What is Travis\'s middle name?', ['Lex', 'William', 'Fitzgerald'], 'Fitzgerald');
+  // const game = new Game;
+  // const deck = new Deck([card1, card2, card3]);
+  // let round;
 
   // beforeEach(function() {
   //   deck = new Deck([card1, card2, card3]);
