c9fddb46919ced47e2f131bb8de17b656f18b429
Anthony Iacono <72999840+anthony-iacono@users.noreply.github.com>
2021-08-25 21:33:49 -0700

    Get game to run in CLI

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 const http = require('http');
-const game = require('./src/Game.js');
+const Game = require('./src/Game.js');
+const game = new Game;
 let app = http.createServer();
 
 // Start the server on port 3000
 app.listen(3000, '127.0.0.1');
 console.log('Node server running on port 3000');
-
-
 game.start();
